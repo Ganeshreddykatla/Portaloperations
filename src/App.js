54873bf1ace7dcada033259a76d50e4221bef29f
Ganeshreddykatla <ganeshreddykatla@gmail.com>
2021-10-25 09:51:02 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Layout from './components/Layout/Layout';
+import Sidebar from './components/Layout/Sidebar';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Layout/>
+    </div>
+  );
+}
+
+export default App;
